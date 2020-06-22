3e9f74bf1146a406b9779046f253ce5370edfc46
sergio_mdza <sergio_mdza@outlook.com>
2020-06-22 16:53:35 -0500

    BD conectada

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var conexion = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "rogercore",
+  host: "bdify1u89j6lgitdzrgc-mysql.services.clever-cloud.com",
+  user: "unaytoag8qonpsd5",
+  password: "nUgoZZjuuWIzsM1Axqbb",
+  database: "bdify1u89j6lgitdzrgc",
   multipleStatements: true
 });
 
